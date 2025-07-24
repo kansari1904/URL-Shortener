95d7b5862c40f0ea68f389a76dc3100c339955b8
Md Khalid Ansari <kansari1904@gmail.com>
2025-07-24 23:10:27 +0530

    changes

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const app = express();
 const PORT = 8000;
 
 //db
-connectDB('mongodb+srv://user_12:khalid12@codegyan.7jtlw.mongodb.net/?retryWrites=true&w=majority&appName=CodeGyan').then(()=>{
-    console.log("Database connected.. ")
-})
+// connectDB('mongodb+srv://user_12:khalid12@codegyan.7jtlw.mongodb.net/?retryWrites=true&w=majority&appName=CodeGyan').then(()=>{
+//     console.log("Database connected.. ")
+// })
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
